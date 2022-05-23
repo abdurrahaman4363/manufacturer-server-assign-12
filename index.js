13f8fb906070d310525373d81162863ed44ba2e7
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-05-23 10:09:09 +0600

    backend is start

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware  manufacturer_tools   H9T4M!34jyx-Pjy
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g84sa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('Mongodb is Connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+  res.send('manufacturer web server is running')
+})
+
+app.listen(port, () => {
+  console.log(`manufacturer listening on port ${port}`)
+})
