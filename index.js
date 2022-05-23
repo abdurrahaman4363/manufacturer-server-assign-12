8076698a544e5c8e7922cf300075a2af0eda58e6
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-05-23 10:19:30 +0600

    mongodb collection create

--- index.js
+++ index.js
@@ -14,13 +14,27 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g84sa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+/* 
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   console.log('Mongodb is Connected')
   // perform actions on the collection object
   client.close();
 });
+ */
 
+async function run(){
+    try{
+       
+        await client.connect();
+        console.log('Mongodb is Connected');
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('manufacturer web server is running')
